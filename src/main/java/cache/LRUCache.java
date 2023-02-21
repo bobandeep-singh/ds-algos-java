90cf91e253ff51a9019ec1f4778eccf31cac96a0
Bobandeep Singh <bobandeep_singh@apple.com>
2023-02-21 17:11:28 +0530

    Implementation of LRU cache

--- src/main/java/cache/LRUCache.java
+++ src/main/java/cache/LRUCache.java
@@ -15,31 +15,88 @@ public class LRUCache<T>{
     }
 
     public T get(String key){
-
-        return null;
+        if(null == key || !hashmap.containsKey(key)){
+            return null;
+        }
+        return hashmap.get(key).value;
     }
 
     public void put(String key, T value){
+        if(null == key) return;
 
+        // check if the key already exist in HM
+        Node presentNode = hashmap.get(key);
+        if(null != presentNode){
+            presentNode.value = value;
+
+            queue.moveNodeToFront(presentNode);
+            return;
+        }
+
+        // check if queue is full
+        if(size == capacity){
+            String rearNodeKey = queue.getRearNodeKey();
+            queue.removeNodeFromRear();
+            hashmap.remove(rearNodeKey);
+            size--;
+        }
+
+        // create a new node
+        Node newNode = new Node(key, value);
+        queue.addNodeToFront(newNode);
+        hashmap.put(key, newNode);
+        size++;
     }
-
     private class DoublyLinkedList{
-        private Node front, prev;
+        private Node front, rear;
         public DoublyLinkedList() {
             this.front = null;
-            this.prev = null;
+            this.rear = null;
+        }
+
+        public void moveNodeToFront(Node node) {
+            if(front == rear){
+                front = rear = node;
+            }
+            node.next.prev = node.prev;
+            node.prev.next = node.next;
+            node.prev = null;
+            node.next = front;
+            front = node;
+        }
+
+        public void removeNodeFromRear() {
+            rear.prev.next = null;
+        }
+
+        public void addNodeToFront(Node node) {
+            if(front == null){
+                front = node;
+            } else{
+                node.next = front;
+                node.prev = null;
+                front = node;
+            }
+        }
+
+        public String getRearNodeKey() {
+            if(rear == null){
+                return null;
+            } else{
+                return rear.key;
+            }
         }
     }
 
     private class Node{
         private String key;
         private T value;
-        private Node prev, next;
-        private Node(String key, T value) {
+        Node prev, next;
+
+        public Node(String key, T value) {
             this.key = key;
             this.value = value;
-            this.prev = null;
-            this.next = null;
+            this.prev = this.next = null;
         }
     }
 
